77ea1f3f794e9a59b9b027cff7d18008554f3985
An Binh <abinh79@gmail.com>
2026-07-15 11:05:42 +0700

    upgrade

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const discordBtn = document.querySelector("#discord-btn");
 const editBtn = document.querySelector("#edit-btn");
+const openSourceBtn = document.querySelector("#open-source");
+const tosBtn = document.querySelector("#tos-btn")
+const helpBtn = document.querySelector("#help-btn")
 
 discordBtn.addEventListener("click", function() {
 
-    window.location.href = "https://discord.gg/2tTEUtEha4";
+    window.open("https://discord.gg/2tTEUtEha4", '_blank');
 
 });
 
@@ -12,5 +15,23 @@ editBtn.addEventListener("click", function() {
     window.location.href = "editor/edit.html";
 
 });
+
+openSourceBtn.addEventListener("click", function() {
+
+    window.open("https://github.com/anbinh0306/inka", '_blank');
+
+})
+
+tosBtn.addEventListener("click", function() {
+
+    window.open("tos/tos.html", '_blank');
+
+})
+
+helpBtn.addEventListener("click", function() {
+
+    window.open("help/help.html", '_blank');
+
+})
 
 console.log("Dep trai vc xD");
